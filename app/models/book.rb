ab25f5a7e7beb0935ad9aea80abf4cf846afeb3a
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2024-01-09 22:13:40 +0900

    fix

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,18 @@ class Book < ApplicationRecord
   scope :costly, ->{where("price>?",3000)}
   scope :written_about, ->(theme){where("namelike?","%#{theme}%")}
   belongs_to :publisher
+
+  has_many :book_authors
+  has_many :authors, through: :book_authors
+
+  validates :name, presence: true
+  validates :name, length: { maximum: 25 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+
+  enum sales_status: {
+    reservation: 0,  # 予約受付
+    now_on_sale: 1, # 発売中
+    end_of_print: 2 # 販売終了
+  }
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_12_27_144358) do
+ActiveRecord::Schema.define(version: 2024_01_08_141710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 2023_12_27_144358) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "book_authors", force: :cascade do |t|
+    t.bigint "book_id", null: false
+    t.bigint "author_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_id"], name: "index_book_authors_on_author_id"
+    t.index ["book_id"], name: "index_book_authors_on_book_id"
+  end
+
   create_table "books", force: :cascade do |t|
     t.string "name"
     t.date "published_on"
@@ -29,6 +38,7 @@ ActiveRecord::Schema.define(version: 2023_12_27_144358) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "publisher_id", null: false
+    t.integer "salses_status"
     t.index ["publisher_id"], name: "index_books_on_publisher_id"
   end
 
@@ -39,5 +49,7 @@ ActiveRecord::Schema.define(version: 2023_12_27_144358) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "book_authors", "authors"
+  add_foreign_key "book_authors", "books"
   add_foreign_key "books", "publishers"
 end
